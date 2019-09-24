8af49a99ca3141518798ea4a6799e68b55c7b9d4
WWStudent <WWStudent@MS-GWSTTF2>
2019-09-24 16:20:57 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ParkIT.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
